528fc2ff5c2539ced50c46bbd0f17cf73283ff7f
n01086924 <myuz97@naver.com>
2018-02-28 17:19:52 -0400

    I added 4 more button and script for control 2 button groups

---
+++ Assets/Code/UI/ButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour {
+	
+	public void RemoveButton() 
+	{
+		GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag ("Buttons");
+
+		foreach(GameObject go in gameObjectArray)
+		{
+			go.SetActive (false);
+		}
+	
+
+	}
+}
