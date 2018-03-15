6a7da0fe9bccb74e77097e55e4e2fa41f4334bee
Brandon <brandongranbrem@gmail.com>
2018-03-15 12:08:10 -0300

    Added prefab for item in the schedule list

---
+++ Assets/Code/ListItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListItem : MonoBehaviour {
+
+    public Button remove;
+    public Text description;
+    public Text date;
+    public Text time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Setup()
+    {
+
+    }
+
+}
