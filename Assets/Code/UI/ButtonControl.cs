ed44f0c02a85d29b35e974bd108fbc8afa927aff
n01086924 <myuz97@naver.com>
2018-03-18 10:03:29 -0300

    Prototype of the exchanging two button groups
    This is for checking the exchanging of two button groups.
    It's still required the code for finding inactive objects

--- Assets/Code/UI/ButtonControl.cs
+++ Assets/Code/UI/ButtonControl.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonControl : MonoBehaviour {
-	
-	public void RemoveButton() 
+
+	GameObject[] gameObjectArray1;
+	GameObject[] gameObjectArray2;
+
+	void Start() {
+		gameObjectArray1 = GameObject.FindGameObjectsWithTag ("Buttons");
+		gameObjectArray2 = GameObject.FindGameObjectsWithTag ("Extra_Buttons");
+	}
+
+	public void ChangeButton(bool buttonGroup) 
 	{
-		GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag ("Buttons");
-
-		foreach(GameObject go in gameObjectArray)
-		{
-			go.SetActive (false);
+		if (buttonGroup) {
+			foreach (GameObject go in gameObjectArray1) {
+				go.SetActive (false);
+			}
+			foreach (GameObject go in gameObjectArray2) {
+				go.SetActive (true);
+			}
 		}
-	
-
+		else {
+			foreach (GameObject go in gameObjectArray1) {
+				go.SetActive (true);
+			}
+			foreach (GameObject go in gameObjectArray2) {
+				go.SetActive (false);
+			}
+		}
 	}
 }
