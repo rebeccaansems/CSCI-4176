ad6a318e385cd63d2ed171201dfbe1456d808f21
BruceMacD <brucewmacdonald@gmail.com>
2018-03-21 14:16:01 -0300

    show beats and remove them on click

---
+++ Assets/Code/RhythmGame/Beat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour {
+    
+    // Which beat in the song does this correspond to
+    public int songBeatPosition;
+    // The beat this was initialized on
+    public int beatInitAdvance;
+
+    // The position of the beat target
+    private float TARGET_Y_POS = 0;
+
+    private Vector2 startPoint;
+    private Vector2 endPoint;
+    private float step;
+    
+    public void ClickedBeat()
+    {
+        // TODO: Find the position of the beat to score points
+        Debug.Log(transform.position);
+        // Remove the beat
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        transform.SetParent(GameObject.Find("Canvas").transform, false);
+        startPoint = transform.position;
+        endPoint = GameObject.Find("Beat Target").transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Remove the beat if it has been missed
+        if (SongPlayer.beatTime > songBeatPosition)
+        {
+            Destroy(gameObject);
+        }
+        
+        // The step function linearly approaches an asymptote at 1, from https://www.gamasutra.com/blogs/YuChao/20170316/293814/Music_Syncing_in_Rhythm_Games.php
+        step = (beatInitAdvance - (songBeatPosition - SongPlayer.beatTime)) / beatInitAdvance;
+        
+		transform.position = Vector2.Lerp(
+            startPoint,
+            endPoint,
+            step
+        );
+    }
+}
--- Assets/Code/RhythmGame/SongPlayer.cs
+++ Assets/Code/RhythmGame/SongPlayer.cs
@@ -6,27 +6,30 @@ public class SongPlayer : MonoBehaviour {
 
     // Stores the song to be played
     public AudioClip audioClip;
-    public GameObject drumBeat;
+    // public GameObject drumBeat;
+    public Beat beatNote;
+    public static float beatTime;
 
     // Seconds per beat to track progress through song
     private float spb;
     private float bpm;
     private float startTime;
     private float trackTime;
-    private int beatNum;
     // Tracks if we are on a new beat
     private int previousBeat = 0;
+    // When was the last beat created?
+    // TODO: Replace with list of times to spawn beats
+    private int previousBeatSpawned = 0;
+    private int beatNum;
     
     void Start () {
         // Song being used currently is 85bpm
         bpm = 85.0f;
-        // Assumes 60fps https://www.gamasutra.com/blogs/YuChao/20170316/293814/Music_Syncing_in_Rhythm_Games.php
+        // seconds per beat https://www.gamasutra.com/blogs/YuChao/20170316/293814/Music_Syncing_in_Rhythm_Games.php
         spb = 60f / bpm;
 
         AudioSource audio = GetComponent<AudioSource>();
         audio.clip = audioClip;
-
-        // Make sure the beat isn't shown to start
 
         // Start tracking time in the track
         startTime = (float)AudioSettings.dspTime;
@@ -41,17 +44,23 @@ public class SongPlayer : MonoBehaviour {
         trackTime = (float)(AudioSettings.dspTime - startTime);
 
         // Find which beat we are on
+        beatTime = trackTime / spb;
         beatNum = (int) Math.Floor(trackTime / spb);
-        
+
         // For debug display on beat, some beats does not show, probably due to update too fast
         if (beatNum > previousBeat)
         {
+            //Debug.Log(beatNum);
             previousBeat = beatNum;
-            drumBeat.SetActive(true);
-        } else
-        {
-            // Hide beat indicator if previously shown
-            drumBeat.SetActive(false);
+
+            // Currently clicking on every second beat
+            if (beatNum > previousBeatSpawned)
+            {
+                beatNote.songBeatPosition = beatNum + 1;
+                beatNote.beatInitAdvance = 1;
+                Instantiate(beatNote);
+                previousBeatSpawned = beatNum + 1;
+            }
         }
     }
 }
